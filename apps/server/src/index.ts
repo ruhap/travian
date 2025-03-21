b8a4c7f5ebf3cc5e90cf150f7ac2d56ebaef8dfa
ruhap <anttivep@gmail.com>
2025-03-21 13:14:48 +0200

    init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+import { Game } from "@travian/game-engine";
+
+const port = 3001;
+
+const app = express();
+
+const game = new Game();
+
+console.log(game.example());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ packages/game-engine/src/game.ts
@@ -0,0 +1,9 @@
+export class Game {
+  constructor() {
+    console.log("Game is created");
+  }
+
+  example() {
+    return "example";
+  }
+}
