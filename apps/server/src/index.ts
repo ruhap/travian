e7e02e89e9d52959512fa25a3a8c7707ccdbba6e
ruhap <anttivep@gmail.com>
2025-03-21 15:16:41 +0200

    add shadcn to client

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,15 @@
+// filepath: c:\Users\root\projektit\travian\apps\server\src\index.ts
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 import { Game } from "@travian/game-engine";
 
 const port = 3001;
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 const game = new Game();
 
@@ -16,6 +21,19 @@ app.get("/game", (req, res) => {
   res.json({ message: game.example() });
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("message", (data) => {
+    console.log("Message received:", data);
+    socket.emit("response", { message: "Message received!" });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
